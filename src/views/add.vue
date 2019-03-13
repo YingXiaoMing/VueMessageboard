50a7604c58ef980892ce865d89e56e7982cf63a9
ljl@nssp.biz <ljl@nssp.biz>
2019-03-13 15:18:15 +0800

    添加2个组件，新增留言和搜索留言组件

---
+++ src/views/add.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    新增留言
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'add',
+  components: {
+  }
+}
+</script>
---
+++ src/views/search.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>查询留言板</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
